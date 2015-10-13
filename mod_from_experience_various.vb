f415e477f6432edb8b723b4a3184e19952f8166d
Vitosh Academy <vitosh.doynov@gmail.com>
2015-10-13 10:32:32 +0200

    Add Last Row and Last Column

--- mod_from_experience_various.vb
+++ mod_from_experience_various.vb
@@ -494,3 +494,32 @@ Public Function b_value_in_array(my_value As Variant, my_array As Variant) As Bo
     b_value_in_array = Not IsError(Application.Match(CStr(my_value), my_array, 0))
     
 End Function
+
+Function last_row(Optional str_sheet As String, Optional column_to_check As Long = 1) As Long
+    
+    Dim shSheet  As Worksheet
+    
+    If str_sheet = vbNullString Then
+        Set shSheet = ActiveSheet
+    Else
+        Set shSheet = Worksheets(str_sheet)
+    End If
+    
+    last_row = shSheet.Cells(shSheet.Rows.Count, column_to_check).End(xlUp).Row
+
+End Function
+
+
+Function last_column(Optional str_sheet As String, Optional row_to_check As Long = 1) As Long
+
+    Dim shSheet  As Worksheet
+    
+    If str_sheet = vbNullString Then
+        Set shSheet = ActiveSheet
+    Else
+        Set shSheet = Worksheets(str_sheet)
+    End If
+    
+    last_column = shSheet.Cells(row_to_check, shSheet.Columns.Count).End(xlToLeft).Column
+    
+End Function
