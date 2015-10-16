22d3d4c050c4cb7bd2fb79cd0c95a849af59dd37
Vitosh Academy <vitosh.doynov@gmail.com>
2015-10-16 12:03:21 +0200

    Create selection_range_trick.vb

---
+++ selection_range_trick.vb
@@ -0,0 +1,34 @@
+Public Sub SelectAndChange()
+        
+    Dim current_cells_range         As Range
+    Dim my_array                    As Variant
+    
+    Dim l_step_between_BA           As Long
+    Dim l_counter                   As Long
+    Dim l_counter_2                 As Long
+    Dim l_counter_3                 As Long
+    Dim col                         As Long
+    Dim row                         As Long
+    
+    l_step_between_BA = 17
+    col = 16
+    row = 33
+    'Beware what you select, for it would stay selected! :)
+    Set current_cells_range = Selection
+    
+    For l_counter = 0 To 9
+        Set current_cells_range = Union(current_cells_range, tbl_Input.Cells(row + l_step_between_BA * l_counter, col))
+        
+        Set current_cells_range = Union(current_cells_range, tbl_Input.Cells(row + 1 + l_step_between_BA * l_counter, col))
+        
+        Set current_cells_range = Union(current_cells_range, tbl_Input.Cells(row + 2 + l_step_between_BA * l_counter, col))
+        
+        Set current_cells_range = Union(current_cells_range, tbl_Input.Cells(row + 3 + l_step_between_BA * l_counter, col))
+                
+        Set current_cells_range = Union(current_cells_range, tbl_Input.Cells(row + 4 + l_step_between_BA * l_counter, col))
+        
+    Next l_counter
+    
+    current_cells_range.Select
+    
+End Sub
