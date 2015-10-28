0bf0e7397a4bfadf81f34127930469fe1c32ee47
Vitosh Academy <vitosh.doynov@gmail.com>
2015-10-28 15:38:16 +0100

    Create IUnitTypes.vb

---
+++ cls_interfaces_2/IUnitTypes.vb
@@ -0,0 +1,17 @@
+Option Explicit
+
+Public Sub Info()
+    
+End Sub
+
+Public Sub WriteTypes()
+    
+End Sub
+
+Public Function CalculatePrice(dbl_m2 As Double, dbl_price_per_m2 As Double) As Double
+    
+End Function
+
+Public Sub WriteOn(str_name As String)
+
+End Sub
