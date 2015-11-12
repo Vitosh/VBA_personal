f283be3171f16f9da301c4fec58e81259656bb80
Vitosh Academy <vitosh.doynov@gmail.com>
2015-11-12 11:03:25 +0100

    Create IncrementDecrement.vb

---
+++ IncrementDecrement.vb
@@ -0,0 +1,11 @@
+Public Sub Increment(ByRef value_to_increment, Optional l_plus As Long = 1)
+    
+    value_to_increment = value_to_increment + l_plus
+
+End Sub
+
+Public Sub Decrement(value_to_decrement, Optional l_minus As Long = 1)
+    
+    value_to_decrement = value_to_decrement - l_minus
+
+End Sub
