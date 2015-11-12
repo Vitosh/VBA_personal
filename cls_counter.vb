6c2031cce60c863c13532aec983789216d7604e3
Vitosh Academy <vitosh.doynov@gmail.com>
2015-11-12 09:32:36 +0100

    Update cls_counter.vb

--- cls_counter.vb
+++ cls_counter.vb
@@ -1,43 +1,77 @@
-Option Explicit
+Sub TDD()
+    
+    Dim specs               As New SpecSuite
+    Dim test_calendar       As New cls_calendar
+    Dim test_plan           As New cls_plan
+    Dim test_counter        As New cls_counter
+    
+    test_calendar.IncrementRow
+    specs.It("cls_c1").Expect(test_calendar.CurrentRow).ToEqual 1
+    
+    test_calendar.IncrementRow
+    test_calendar.CurrentRow = 5
+    test_calendar.IncrementRow
+    specs.It("cls_c2").Expect(test_calendar.CurrentRow).ToEqual 6
+    
+    test_calendar.LeftDate = "01.09.2015"
+    test_calendar.RightDate = "01.08.2021"
+    specs.It("cls_c3").Expect(test_calendar.Duration).ToEqual 71
+    
+    test_plan.LastLines_Row = 30
+    test_plan.LastLines_Row = 31
+    test_plan.LastLines_Row = 1000
+    specs.It("plan_c4").Expect(test_plan.LastLines_Row(1)).ToEqual 30
+    specs.It("plan_c5").Expect(test_plan.LastLines_Row(2)).ToEqual 31
+    specs.It("plan_c6").Expect(test_plan.LastLines_Row(3)).ToEqual 1000
+    
+    specs.It("plan_c7").Expect(test_plan.LastLines_Row_Count).ToEqual 3
+    specs.It("plan_c8").Expect(test_plan.LastLines_Row_Count).ToNotEqual 4
+    
+    test_counter.IncrementCounter
+    test_counter.IncrementCounter
+    specs.It("counter_c9").Expect(test_counter.Counter).ToEqual 2
+    specs.It("counter_c10").Expect(test_counter.Counter).ToNotEqual 3
+    
+    test_counter.ResetCounter
+    specs.It("counter_c11").Expect(test_counter.Counter).ToEqual 0
+    
+    test_counter.IncrementCounter (10)
+    specs.It("counter_c12").Expect(test_counter.Counter).ToEqual 10
+    
+    test_counter.IncrementCounter
+    specs.It("counter_c12a").Expect(test_counter.Counter).ToEqual 11
+    
+    test_counter.DecrementCounter
+    specs.It("counter_c12b").Expect(test_counter.Counter).ToEqual 10
+    
+    test_counter.ResetCounter
+    test_counter.DecrementCounter
+    specs.It("counter_c12c").Expect(test_counter.Counter).ToEqual -1
 
-Private p_counter_value         As Long
-Private p_flagged               As Variant
-
-Public Sub IncrementCounter(Optional value As Long = 1)
-    p_counter_value = p_counter_value + value
-    ReDim Preserve p_flagged(UBound(p_flagged) + 1)
+    test_counter.ResetCounter
+    test_counter.IncrementCounter
+    test_counter.Flag
+    specs.It("counter_c13").Expect(test_counter.IsFlagged).ToEqual True
+    
+    test_counter.IncrementCounter
+    specs.It("counter_c14").Expect(test_counter.IsFlagged).ToEqual False
+    
+    test_counter.Flag
+    specs.It("counter_c15").Expect(test_counter.IsFlagged).ToEqual True
+    
+    test_counter.UnFlag
+    specs.It("counter_c16").Expect(test_counter.IsFlagged).ToEqual False
+    
+    InlineRunner.RunSuite specs
+    
+    Set test_calendar = Nothing
+    Set test_plan = Nothing
+    Set specs = Nothing
+    Set test_counter = Nothing
+    
 End Sub
 
-Public Sub ResetCounter(Optional value As Long = 1)
-    p_counter_value = value
-    Call ResetFlagsArray
-End Sub
-
-Public Property Get Counter() As Long
-    Counter = p_counter_value
-End Property
-
-Public Sub Flag()
-    p_flagged(Counter) = True
-End Sub
-
-Public Sub UnFlag()
-    p_flagged(Counter) = False
-End Sub
-
-Public Property Get IsFlagged() As Boolean
-    IsFlagged = p_flagged(Counter)
-End Property
-
-Private Sub Class_Initialize()
-    Call ResetFlagsArray
-End Sub
-
-Public Sub ResetFlagsArray()
-    ReDim p_flagged(1)
-End Sub
-
-'TESTS ARE HERE:
+'Unit tests are here:
 Sub TDD()
     
     Dim specs               As New SpecSuite
