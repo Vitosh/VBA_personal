2ed75b8d1623be4909db46c7f98a47ca1a62777b
Vitosh Academy <vitosh.doynov@gmail.com>
2015-12-02 12:09:50 +0100

    Update Timer.vb

--- Timer.vb
+++ Timer.vb
@@ -2,6 +2,8 @@
 Public dbl_timer                        As Double
 
 'On Start:
+'[set_execution_time] = "Durchführungszeit: "
+
 Sub SetPublicConstants()
     
     dbl_timer = timer
@@ -14,4 +16,14 @@ Public Sub ShowMsgBoxReady()
     MsgBox "Fertig" & vbCrLf & Round(timer - dbl_timer) & "Sekunden!", vbInformation, "Nachricht"
 
 End Sub
+
+Public Sub ShowMsgBoxReady()
+    
+    Dim str_time As String
+    
+    str_time = [set_execution_time] & (timer - dbl_timer) \ 60 & ":" & (timer - dbl_timer) Mod 60
+    MsgBox "Fertig!" & vbCrLf & str_time, vbInformation, "Nachricht"
+
+End Sub
+
 
