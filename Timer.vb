11adf6df85fbf50b816cb313384a7a0471337d3f
Vitosh Academy <vitosh.doynov@gmail.com>
2015-12-02 15:17:07 +0100

    Update Timer.vb

--- Timer.vb
+++ Timer.vb
@@ -11,17 +11,33 @@ Sub SetPublicConstants()
 End Sub
 
 'At the end
+'Option 1
 Public Sub ShowMsgBoxReady()
 
     MsgBox "Fertig" & vbCrLf & Round(timer - dbl_timer) & "Sekunden!", vbInformation, "Nachricht"
 
 End Sub
 
+'Option 2
 Public Sub ShowMsgBoxReady()
     
     Dim str_time As String
     
     str_time = [set_execution_time] & (timer - dbl_timer) \ 60 & ":" & (timer - dbl_timer) Mod 60
+    MsgBox "Fertig!" & vbCrLf & str_time, vbInformation, "Nachricht"
+
+End Sub
+
+'Option 3
+Public Sub ShowMsgBoxReady()
+    
+    Dim str_time                As String
+    Dim lng_result              As Long
+    
+    lng_result = Timer - dbl_timer
+      
+    
+    str_time = [set_execution_time] & lng_result \ 60 & ":" & IIf(Len(CStr(lng_result Mod 60)) < 2, "0", "") & (lng_result) Mod 60
     MsgBox "Fertig!" & vbCrLf & str_time, vbInformation, "Nachricht"
 
 End Sub
