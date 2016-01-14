8ea17e0e542c3d095a363a2629f77339ca58c904
Vitosh Academy <vitosh.doynov@gmail.com>
2016-01-14 14:47:01 +0100

    Update remove_spaces.vb

--- remove_spaces.vb
+++ remove_spaces.vb
@@ -3,10 +3,10 @@ Public Sub remove_space_in_string()
     Dim r_range As Range
         
     For Each r_range In Selection
-        r_range.Value = RTrim(r_range.Value)
-        r_range.Cells.Font.Bold = False
+        r_range = Trim(r_range)
+        r_range = Replace(r_range, vbTab, "")
         r_range = Replace(r_range, " ", "")
-        
+        r_range = Replace(r_range, Chr(160), "")
     Next r_range
 
 End Sub
