31a8f5aeeae3eef76b1e53187e3227c56d2adcc6
Vitosh Academy <vitosh.doynov@gmail.com>
2016-01-15 11:33:26 +0100

    Update GetWorkbook.vb

--- GetWorkbook.vb
+++ GetWorkbook.vb
@@ -17,3 +17,22 @@ Public Function GetWorkbook(ByVal sFullName As String) As Workbook
     Set GetWorkbook = wbReturn
     
 End Function
+
+Public Function calculate_range(from_row As Long, to_row As Long, l_column As Long, _
+                                Optional s_sheet_name As String = "calendar") As Double
+
+    Dim ws              As Worksheet
+    Dim l_counter       As Long
+    Dim d_result        As Double
+    
+    Set ws = ThisWorkbook.Worksheets(s_sheet_name)
+    
+    For l_counter = from_row To to_row
+        Call Increment(d_result, ws.Cells(l_counter, l_column))
+    Next l_counter
+
+    Set ws = Nothing
+    
+    calculate_range = Round(d_result, 2)
+    
+End Function
