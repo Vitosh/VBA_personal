8d6ce46acc4499c4fd04159f32fdb082392314e2
Vitosh Academy <vitosh.doynov@gmail.com>
2016-01-21 15:44:15 +0100

    Create remove_msgbox.vb

---
+++ remove_msgbox.vb
@@ -0,0 +1,9 @@
+
+For Excel 2007 Version :
+You can follow the following steps to avoide the privacy warning.
+1 Menu Button "on the top left of the excel window"
+2 Excel Option
+3 Trust Center
+4 Trust Center setting
+5 Privacy Options 
+6 unmark the "Remove personal information from file properties on save"
