81edca5a4ac2d871f6301b5045717972348b654e
Doynov <v.doynov@prj-nue-mail.com>
2016-01-28 11:53:32 +0100

    add goto_internet

---
+++ goto_internet.vb
@@ -0,0 +1,35 @@
+Public Sub Clicked(Optional b_logo As Boolean = False)
+
+    Dim ie                  As Object
+    Dim s_WebSites()        As Variant
+    
+   On Error GoTo Clicked_Error
+
+    If b_logo Then
+    
+        s_WebSites = Array("https://www.facebook.com", _
+                               "https://plus.google.com", _
+                               "http://www.youtube.com")
+    Else
+        s_WebSites = Array("http://www.hoai.de/online/hoai_rechner")
+    End If
+     
+'    s_WebSites = Array("https://goo.gl/c3Gzqi", _
+'                        "https://goo.gl/JKvYR6", _
+'                        "https://goo.gl/eLuMFN", _
+'                        "https://goo.gl/r2OMeQ")
+                
+    Set ie = CreateObject("Internetexplorer.Application")
+    ie.Visible = True
+    ie.Navigate s_WebSites(make_random(0, UBound(s_WebSites)))
+
+   Exit Sub
+
+   On Error GoTo 0
+   Exit Sub
+
+Clicked_Error:
+
+    MsgBox "Error " & Err.Number & " (" & Err.Description & ") in procedure Clicked of Module mod_main"
+    
+End Sub
