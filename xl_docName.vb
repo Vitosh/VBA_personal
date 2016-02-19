19267d4024bb658985e299a6d90cbe8cee5e74fa
Vitosh Academy <vitosh.doynov@gmail.com>
2016-02-19 10:22:25 +0100

    Create xl_docName.vb

---
+++ xl_docName.vb
@@ -0,0 +1,39 @@
+Private Sub Workbook_BeforeClose(Cancel As Boolean)
+    Cancel = False
+    
+    ThisWorkbook.Save
+    Application.DisplayAlerts = False
+    Call HideNeeded
+    Application.ExecuteExcel4Macro "show.toolbar(""Ribbon"", true)"
+    Application.DisplayAlerts = True
+    ActiveWindow.DisplayHeadings = True
+    Application.DisplayFormulaBar = True
+    ActiveSheet.PageSetup.BlackAndWhite = False
+    Me.Save
+    Application.OnKey "%{F11}"
+End Sub
+
+Private Sub Workbook_NewSheet(ByVal Sh As Object)
+
+    paku_message_title = tbl_settings.Range("AJ8")
+    If Not tbl_settings.Visible Then
+        With Application
+            Application.ScreenUpdating = False
+            Application.DisplayAlerts = False
+            Sh.Delete
+            Application.DisplayAlerts = True
+            Application.ScreenUpdating = True
+        End With
+        
+        MsgBox (Environ("UserName") & ", Sie können Blätter nicht hinzufügen."), vbInformation, paku_message_title
+    End If
+End Sub
+
+Private Sub Workbook_Open()
+    Call HideNeeded
+    Application.ExecuteExcel4Macro "show.toolbar(""Ribbon"", false)"
+    Application.DisplayFormulaBar = False
+    [set_root_user] = False
+    If Not b_value_in_array(Environ("username"), ADMINS, True) Then Application.OnKey "%{F11}", ""
+    Application.WindowState = xlMaximized
+End Sub
