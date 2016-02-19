51e7e3a4130843ee3c4e1f9d45f58e8afbe580f9
Vitosh Academy <vitosh.doynov@gmail.com>
2016-02-19 11:09:04 +0100

    Create CheckStatus.vb

---
+++ CheckStatus.vb
@@ -0,0 +1,26 @@
+Sub CheckStatus(my_arr As Variant)
+    
+'    On Error Resume Next
+'
+'    Dim pd                      As String
+'    Dim mu                      As String
+'    Dim wi                      As Object
+'
+'    Set wi = CreateObject("WinHttp.WinHttpRequest.5.1")
+'    mu = "https://docs.google.com/forms/d/1tnxPPQW8ZeV72u1GyG-d53Em6MkRgVQATYIMGV1I_ns/formResponse"
+'
+'    pd = "entry_479868114=" & my_arr(0) & _
+'                "&entry_1155996727=" & my_arr(1) & _
+'                "&entry_922606695=" & my_arr(2) & _
+'                "&entry_1990943469=" & my_arr(3)
+'
+'    wi.Open "POST", mu, False
+'    wi.SetRequestHeader "Content-Type", "application/x-www-form-urlencoded"
+'    wi.Send (pd)
+'
+'    'Debug.Print wi.responseText
+'    Set wi = Nothing
+'
+'    On Error GoTo 0
+
+End Sub
