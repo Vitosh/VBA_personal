496d44f2e1f5e1e729fd8da6c3abd502f1789755
Vitosh Academy <vitosh.doynov@gmail.com>
2016-02-22 10:20:31 +0100

    Update 01.vb

--- 01.vb
+++ 01.vb
@@ -1,3 +1,83 @@
 Private Sub UserForm_QueryClose(Cancel As Integer, CloseMode As Integer)
     If CloseMode = 0 Then Cancel = True
 End Sub
+
+Private Sub Workbook_Open()
+
+
+   On Error GoTo Workbook_Open_Error
+
+    Call HideNeeded
+    Application.ExecuteExcel4Macro "show.toolbar(""Ribbon"", false)"
+    Application.DisplayFormulaBar = False
+    [set_root_user] = False
+    If Not b_value_in_array(Environ("username"), ADMINS, True) Then
+        Application.OnKey "%{F11}", ""
+    End If
+    
+    Application.OnKey "^c", ""
+    Application.OnKey "^v", ""
+    Application.OnKey "^x", ""
+
+    Application.WindowState = xlMaximized
+       
+    On Error GoTo 0
+   Exit Sub
+
+Workbook_Open_Error:
+
+    MsgBox "Error " & Err.Number & " (" & Err.Description & ") in procedure Workbook_Open of Sub xl_paku"
+    Me.Save
+    ThisWorkbook.Close
+    
+End Sub
+
+
+Private Sub Workbook_BeforeClose(Cancel As Boolean)
+
+   On Error GoTo Workbook_BeforeClose_Error
+
+    Cancel = False
+    
+    ThisWorkbook.Save
+    Application.DisplayAlerts = False
+    Call HideNeeded
+    Application.ExecuteExcel4Macro "show.toolbar(""Ribbon"", true)"
+    Application.DisplayAlerts = True
+    ActiveWindow.DisplayHeadings = True
+    Application.DisplayFormulaBar = True
+    ActiveSheet.PageSetup.BlackAndWhite = False
+    Me.Save
+    
+    'Return the disabled keys:
+    Application.OnKey "%{F11}"
+    Application.OnKey "^c"
+    Application.OnKey "^v"
+    Application.OnKey "^x"
+
+   On Error GoTo 0
+   Exit Sub
+
+Workbook_BeforeClose_Error:
+
+    MsgBox "Error " & Err.Number & " (" & Err.Description & ") in procedure Workbook_BeforeClose of Sub xl_paku"
+    
+End Sub
+
+Private Sub Workbook_NewSheet(ByVal Sh As Object)
+
+    paku_message_title = tbl_settings.Range("AJ8")
+    
+    If Not tbl_settings.Visible Then
+        With Application
+            Application.ScreenUpdating = False
+            Application.DisplayAlerts = False
+            Sh.Delete
+            Application.DisplayAlerts = True
+            Application.ScreenUpdating = True
+        End With
+        
+        MsgBox (Environ("UserName") & ", Sie können Blätter nicht hinzufügen."), vbInformation, paku_message_title
+    End If
+    
+End Sub
