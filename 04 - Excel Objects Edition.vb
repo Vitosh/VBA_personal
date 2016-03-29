11939e86281a6f6d8d4e86228bc53994fe5d9faf
Vitosh Academy <vitosh.doynov@gmail.com>
2016-03-29 15:58:08 +0200

    Update 04 - Excel Objects Edition.vb

--- 04 - Excel Objects Edition.vb
+++ 04 - Excel Objects Edition.vb
@@ -1,3 +1,57 @@
+Option Explicit
+
+Sub RemoveFormulasFromAnotherSheet()
+    
+    Dim rng_cell            As Range
+    Dim str_inside          As String: str_inside = ":\"
+    
+    For Each rng_cell In ActiveSheet.UsedRange 'Selection
+        If InStr(rng_cell.Formula, str_inside) > 0 Then
+            Debug.Print rng_cell.Formula
+            Debug.Print rng_cell.Address
+            Debug.Print "---------------------------"
+            'rng_cell.Value = rng_cell.Value
+        End If
+    Next rng_cell
+End Sub
+
+Sub ExtendContentFromRight()
+    
+    Dim rng_first           As Range
+
+    Set rng_first = Selection.Cells(1, 1)
+    
+    Selection.Formula = rng_first.Formula
+    
+    Set rng_first = Nothing
+    
+ End Sub
+
+Public Sub ColorSS()
+    
+    On Error GoTo ColorSS_Error
+    
+    'Colors Saturdays and Sundays.
+    
+    Dim r_cell      As Range
+    Dim r_range     As Range
+    
+    For Each r_cell In Selection
+        If Weekday(r_cell.Value) = 1 Or Weekday(r_cell.Value) = 7 Then
+            Set r_range = ActiveSheet.Range(Cells(4, r_cell.Column), Cells(667, r_cell.Column))
+            r_range.Interior.Color = 13434828
+        End If
+    Next r_cell
+    
+    Set r_range = Nothing
+
+    On Error GoTo 0
+    Exit Sub
+
+ColorSS_Error:
+    MsgBox "Error " & Err.Number & " (" & Err.Description & ") in procedure ColorSS of Sub mod_play_with_me"
+End Sub
+
 '---------------------------------------------------------------------------------------
 ' Method : AddStringToFormula
 ' Author : v.doynov
