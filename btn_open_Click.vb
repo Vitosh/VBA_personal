161ccb4ac63905bae60c0c21eb4bcb0ea9cbae8d
Vitosh Academy <vitosh.doynov@gmail.com>
2016-06-09 15:08:27 +0200

    Update btn_open_Click.vb

--- btn_open_Click.vb
+++ btn_open_Click.vb
@@ -2,15 +2,18 @@
 'Eliminates the file name and opens the folder.
 
 Private Sub btn_open_Click()
-     On Error GoTo btn_open_Click_Error
+    On Error GoTo btn_open_Click_Error
      
-     Dim my_str As String
-     
-     my_str = tbl_input.lblDisplayTerminPlanner
-     
-     Call Shell("explorer.exe" & " " & Left(my_str, Len(my_str) - Len(Split(my_str, "\")(UBound(Split(my_str, "\"))))), vbNormalFocus)
+    Dim my_str          As String
+    Dim my_str2         As String
 
+         
+    my_str = tbl_input.lblDisplayTerminPlanner
+    my_str2 = Left(my_str, Len(my_str) - Len(Split(my_str, "\")(UBound(Split(my_str, "\")))))
+    Call Shell("explorer.exe" & " " & my_str2, vbNormalFocus)
+  
 btn_open_Click_Error:
-    Debug.Print Err.Number & " (" & Err.Description & ")"
+    Debug.Print "Error " & Err.Number & " (" & Err.Description & ")"
+
     
 End Sub
