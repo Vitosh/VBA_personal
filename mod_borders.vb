a9b74ac3e235a0bfff57ec9bf70e5de956347456
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-08-12 11:09:12 +0200

    Create mod_borders.vb

---
+++ mod_borders.vb
@@ -0,0 +1,31 @@
+Option Explicit
+
+Sub MakeSelectionWithCells(my_range As Range)
+
+    Dim l_line_style        As Long: l_line_style = 1
+    Dim l_theme_color       As Long: l_theme_color = 2
+    Dim d_tint_shade        As Double: d_tint_shade = 0.349986266670736
+    Dim l_weight            As Long: l_weight = 2
+    Dim l_counter           As Long
+    
+    For l_counter = 7 To 12
+        Call MakeSelectionWithCells_Separated(l_line_style, l_theme_color, d_tint_shade, l_weight, l_counter, my_range)
+    Next l_counter
+    
+End Sub
+
+Public Sub MakeSelectionWithCells_Separated(l_line_style As Long, _
+                                            l_theme_color As Long, _
+                                            d_tint_shade As Double, _
+                                            l_weight As Long, _
+                                            l_counter As Long, _
+                                            my_range As Range)
+                                            
+    With my_range.Borders(l_counter)
+        .LineStyle = l_line_style
+        .ThemeColor = l_theme_color
+        .TintAndShade = d_tint_shade
+        .Weight = l_weight
+    End With
+    
+End Sub
