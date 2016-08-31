c361f8b5e638736781230fa5bdc6e817e315fc29
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-08-31 10:41:44 +0200

    Create FileExists.vb

---
+++ FileExists.vb
@@ -0,0 +1,10 @@
+Public Function b_file_exists(ByVal str_file_path As String) As Boolean
+
+    Dim str_test    As String
+    
+    On Error Resume Next
+    str_test = Dir(str_file_path)
+    On Error GoTo 0
+    b_file_exists = (str_test <> "")
+
+End Function
