fd37c345141a97e960d125b45e7e4725d044dd2a
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-09-02 09:55:08 +0200

    Update sql_craziness.sql

--- SQL VBA/sql_craziness.sql
+++ SQL VBA/sql_craziness.sql
@@ -1,15 +1,18 @@
-WITH PartitionedCounted AS
+Use Gladiator
+GO
+
+WITH PartitionedCounted AS 
 (
-    SELECT ROW_NUMBER() OVER(PARTITION BY Objekt, EOMONTH(DATUM) ORDER BY DATUM) AS Nr
-          ,EOMONTH(DATUM) AS EOM
-          ,Datum
-          ,Objekt
-    FROM tempt_report AS tbl
+    SELECT ROW_NUMBER() OVER(PARTITION BY Objekt, EOMONTH(DATUM) ORDER BY DATUM) [Nr]
+	,EOMONTH(DATUM) [EOM]
+	,Datum
+	,Objekt
+	FROM tempt_report AS tbl
 )
 
 SELECT pc.EOM [Date],pc.Objekt,tbl.Datum [Initial Date]
 FROM PartitionedCounted AS pc
-INNER JOIN tempt_report AS tbl ON tbl.Objekt=pc.Objekt AND 
+INNER JOIN tempt_report AS tbl ON tbl.Objekt=pc.Objekt AND
 EOMONTH(TBL.Datum)=pc.EOM
 WHERE pc.Nr > 1
 ORDER BY Objekt
