635e5f4e5038f391803f7275e1847b52cdc2616d
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-09-07 13:59:40 +0200

    adding str_connection_string

--- SQL VBA/SQL_Local_Info.vb
+++ SQL VBA/SQL_Local_Info.vb
@@ -6,3 +6,11 @@ Servername:
 
 Authentifizierung:
 Windows-Authentifizierung
+
+str_connection_string = "Provider=SQLNCLI11;Server=(localdb)\MSSQLLocalDB;Initial Catalog=Tempt;Trusted_Connection=yes;timeout=30;"
+
+str_connection_string = "Provider=" & arr_info(0) & _
+  "; Data Source=" & arr_info(1) & _
+  "; Database=" & str_generator(arr_info(2), True) & _
+  ";User ID=" & str_generator(arr_info(3), True) & _
+  "; Password=" & str_generator(arr_info(4), True) & ";"
