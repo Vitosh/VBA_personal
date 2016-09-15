6b5fd023c13acbf7674fe7a0311f64e34a6faa4a
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-09-15 10:12:53 +0200

    Update sql various.vb

--- SQL VBA/sql various.vb
+++ SQL VBA/sql various.vb
@@ -45,3 +45,59 @@ DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,
 DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,
 DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT, 
 DEFAULT);
+
+
+if  exists (select * from sys.objects) 
+	select 'The sys.objects exists'
+else
+	select 'The sys.objects does not exist'
+
+GO
+
+--BACKUP DB
+select * from sys.fn_builtin_permissions(default)
+
+DECLARE @databasename AS NVARCHAR(MAX)
+	, @timecomponent AS NVARCHAR(MAX)
+	, @sqlcommand AS NVARCHAR(MAX);
+	
+SET @databasename = (SELECT MIN(name) FROM sys.databases WHERE name 
+    NOT IN ('master', 'model', 'msdb', 'tempdb'));
+	
+WHILE @databasename IS NOT NULL
+BEGIN
+  SET @timecomponent = REPLACE(REPLACE(REPLACE(CONVERT(NVARCHAR, 
+    GETDATE(), 120), ' ', '_'), ':', ''), '-', '');
+	
+  SET @sqlcommand = 'BACKUP DATABASE ' + @databasename + ' TO DISK =
+       ''C:\Backups\' + @databasename + '_' + @timecomponent + '.bak''';
+	   
+  PRINT @sqlcommand;
+  
+  EXEC(@sqlcommand);
+  
+  SET @databasename = (SELECT MIN(name) FROM sys.databases WHERE name 
+    NOT IN ('master', 'model', 'msdb', 'tempdb') AND name > @databasename);
+END;
+
+GO
+-- 
+Automatic insert of data
+DECLARE @i AS int = 1;
+WHILE @i < 1000000
+BEGIN
+SET @i = @i + 1;
+INSERT INTO dbo.TestStructure
+(id, filler1, filler2)
+VALUES
+(@i, 'a', 'b');
+END;
+GO
+
+select * from dbo.TestStructure
+--
+--clearing cache
+DBCC FREEPROCCACHE
+GO
+--
+
