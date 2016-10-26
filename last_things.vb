15d58fa83ee21c2efeb333048c9920eccc18c475
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-10-26 13:27:40 +0200

    Update last_things.vb

--- last_things.vb
+++ last_things.vb
@@ -27,12 +27,64 @@ Function last_row(Optional str_sheet As String, Optional column_to_check As Long
 
 End Function
 
-Public Function locate_value_col(str_value As String, ByRef target_sheet As Worksheet) As Long
+Public Function l_locate_value_row(target As String, ByRef target_sheet As Worksheet, _
+                                   Optional l_col As Long = 1, _
+                                   Optional l_more_values_found As Long = 1, _
+                                   Optional b_look_for_part = False) As Long
+
+    Dim l_values_found  As Long
+    Dim r_local_range   As Range
+    Dim my_cell         As Range
     
-    Dim cell_to_find As Range
+    l_values_found = l_more_values_found
+
+    Set r_local_range = target_sheet.Range(target_sheet.Cells(1, l_col), target_sheet.Cells(Rows.Count, l_col))
+
+    For Each my_cell In r_local_range
+
+        'The b_look_for_part is for the vertriebscase
+        If b_look_for_part Then
+            If target = Left(my_cell, Len(target)) Then
+                If l_values_found = 1 Then
+                    l_locate_value_row = my_cell.Row
+                    Exit Function
+                Else
+                    Call Decrement(l_values_found)
+                End If
+            End If
+        Else
+            If target = Trim(my_cell) Then
+                If l_values_found = 1 Then
+                    l_locate_value_row = my_cell.Row
+                    Exit Function
+                Else
+                    Call Decrement(l_values_found)
+                End If
+            End If
+        End If
+    Next my_cell
+
+    l_locate_value_row = -1
+
+End Function
+
+Public Function l_locate_value_col(target As String, _
+                                    ByRef target_sheet As Worksheet, _
+                                    Optional l_row As Long = 1)
+
+    Dim cell_to_find                As Range
+    Dim r_local_range               As Range
+    Dim my_cell                     As Range
     
-    Set cell_to_find = Range(target_sheet.Cells(1, 1), target_sheet.Cells(1, last_col(tbl_totals))).Find(str_value, LookIn:=xlValues)
-    locate_value_col = cell_to_find.Column
-    Set cell_to_find = Nothing
+    Set r_local_range = target_sheet.Range(target_sheet.Cells(l_row, 1), target_sheet.Cells(l_row, Columns.Count))
     
+    For Each my_cell In r_local_range
+        If target = Trim(my_cell) Then
+            l_locate_value_col = my_cell.Column
+            Exit Function
+        End If
+    Next my_cell
+    
+    l_locate_value_col = -1
+
 End Function
