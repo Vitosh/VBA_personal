232c5515e2227bdfcb26215409c03607642088b1
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-10-31 16:23:33 +0100

    Update StyleKiller.vb

--- StyleKiller.vb
+++ StyleKiller.vb
@@ -1,3 +1,5 @@
+Option Explicit
+
 Sub StyleKiller()
 
     Dim my_style                As Style
@@ -9,4 +11,37 @@ Sub StyleKiller()
         End If
     Next my_style
 
+End Sub
+
+'FANCY ONE:
+'**************************************************************************************
+Sub RemoveTheStyles()
+
+    Dim style               As style
+    Dim l_counter           As Long
+    Dim l_total_number      As Long
+
+    On Error Resume Next
+
+    l_total_number = ActiveWorkbook.Styles.Count
+    Application.ScreenUpdating = False
+
+    For l_counter = l_total_number To 1 Step -1
+    
+        Set style = ActiveWorkbook.Styles(l_counter)
+        
+        If (l_counter Mod 500 = 0) Then
+            DoEvents
+            Application.StatusBar = "Deleting " & l_total_number - l_counter + 1 & " of " & l_total_number & " " & style.Name
+        End If
+        
+        If Not style.BuiltIn Then style.Delete
+
+    Next l_counter
+
+    Application.ScreenUpdating = True
+    Application.StatusBar = False
+    Debug.Print "READY!"
+    
+    On Error GoTo 0
 End Sub
