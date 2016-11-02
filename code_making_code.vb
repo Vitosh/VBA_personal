3b1edf9f13b861ce31e73f45260173d4efa5d0e5
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-11-02 17:20:35 +0100

    Create code_making_code.vb

---
+++ code_making_code.vb
@@ -0,0 +1,20 @@
+Public Sub TakeValues()
+
+    Dim my_cell         As Range
+    Dim str             As String
+    Dim l_counter       As Long
+    
+    For Each my_cell In Selection
+        Call Increment(l_counter)
+        str = "my_arr(" & l_counter & ")= "
+        If Len(my_cell) > 1 Then
+            str = str & change_commas(my_cell.value)
+        Else
+            str = str & 0
+        End If
+        
+        Debug.Print str
+        
+    Next my_cell
+
+End Sub
