2559d953d1eb16a8486e56fc4b55780b8f3b1fd3
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-12-09 13:49:12 +0100

    Update NamedRanges.vb

--- NamedRanges.vb
+++ NamedRanges.vb
@@ -24,3 +24,47 @@ Sub change_all_names()
     Next i
 
 End Sub
+
+Option Explicit
+
+Public Sub MakeNegativesOne(l_col As Long)
+
+    Dim l_counter           As Long
+    Dim b_negative          As Long
+    Dim my_cell             As Range
+    Dim my_first_negative   As Range
+    
+    Dim dbl_negative_sum    As Double
+    
+    For l_counter = 1 To 13
+        Set my_cell = Cells(l_col, l_counter)
+        
+        If my_cell < 0 And my_cell.HasFormula Then
+            dbl_negative_sum = dbl_negative_sum + my_cell.Value
+            
+            If Not b_negative Then
+                b_negative = True
+                Set my_first_negative = my_cell
+            End If
+            
+            my_cell = 0
+        End If
+    Next l_counter
+    
+    If b_negative Then
+        my_first_negative = dbl_negative_sum
+    End If
+    
+End Sub
+
+Public Sub NegativeSelection(Optional my_rng As Variant)
+
+    Dim my_cell As Range
+
+    If IsMissing(my_rng) Then Set my_rng = Selection
+    
+    For Each my_cell In my_rng
+        my_cell = my_cell * -1
+    Next my_cell
+
+End Sub
