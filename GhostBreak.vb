381ebc6d90e324e30946c534e5cb1c424b56cf5b
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-12-14 09:55:16 +0100

    Update GhostBreak.vb

--- GhostBreak.vb
+++ GhostBreak.vb
@@ -1,4 +1,5 @@
 'http://stackoverflow.com/questions/2154699/excel-vba-app-stops-spontaneously-with-message-code-execution-has-been-halted
+'Ghost break unwanted break freezing
 
 Press "Debug" button in the popup.
 Press Ctrl+Pause|Break twice.
