720a27fe2141faa04d0eea2a9d815602a32b4b24
Vitosh Academy <Vitosh@users.noreply.github.com>
2017-03-16 17:16:04 +0100

    Update last_things.vb

--- last_things.vb
+++ last_things.vb
@@ -63,17 +63,24 @@ Public Function LastUsedRow() As Long
 
 End Function
 
-Public Function fnLngLocateValueRow(ByVal strTarget, _
-                                    ByRef wksTarget As Worksheet, _
-                                    Optional lngCol As Long = 1, _
-                                    Optional lngMoreValuesFound As Long = 1, _
-                                    Optional blnLookForPart = False, _
-                                    Optional blnLookUpToBottom = True) As Long
+'---------------------------------------------------------------------------------------
+' Procedure : fnLngLocateValueRow
+' Author    : v.doynov
+' Date      : 16.03.2017
+' Purpose   : blnLookUpToBottom is more powerful than lngMoreValuesFound and makes it useless.
+'---------------------------------------------------------------------------------------
+'
+Public Function fnLngLocateValueRow(ByVal strTarget As String, _
+    ByRef wksTarget As Worksheet, _
+    Optional lngCol As Long = 1, _
+    Optional lngMoreValuesFound As Long = 1, _
+    Optional blnLookForPart = False, _
+    Optional blnLookUpToBottom = True) As Long
 
     Dim lngValuesFound      As Long
     Dim rngLocal            As Range
     Dim rngMyCell           As Range
-    
+
     fnLngLocateValueRow = -999
     lngValuesFound = lngMoreValuesFound
     Set rngLocal = wksTarget.Range(wksTarget.Cells(1, lngCol), wksTarget.Cells(Rows.Count, lngCol))
@@ -102,25 +109,34 @@ Public Function fnLngLocateValueRow(ByVal strTarget, _
 
 End Function
 
-Public Function fnLngLocateValueCol(ByVal strTarget, _
-                                    ByRef wksTarget As Worksheet, _
-                                    Optional lngRow As Long = 1, _
-                                    Optional lngMoreValuesFound As Long = 1, _
-                                    Optional blnLookForPart = False) As Long
-                                    
+'---------------------------------------------------------------------------------------
+' Procedure : fnLngLocateValueCol
+' Author    : v.doynov
+' Date      : 16.03.2017
+' Purpose   : blnLookUpToBottom is more powerful than lngMoreValuesFound and makes it useless.
+'---------------------------------------------------------------------------------------
+'
+Public Function fnLngLocateValueCol(ByVal strTarget As String, _
+    ByRef wksTarget As Worksheet, _
+    Optional lngRow As Long = 1, _
+    Optional lngMoreValuesFound As Long = 1, _
+    Optional blnLookForPart = False, _
+    Optional blnLookUpToBottom = True) As Long
+
     Dim lngValuesFound          As Long
     Dim rngLocal                As Range
     Dim rngMyCell               As Range
-    
+
+    fnLngLocateValueCol = -999
     lngValuesFound = lngMoreValuesFound
     Set rngLocal = wksTarget.Range(wksTarget.Cells(lngRow, 1), wksTarget.Cells(lngRow, Columns.Count))
-    
+
     For Each rngMyCell In rngLocal
         If blnLookForPart Then
             If strTarget = Left(rngMyCell, Len(strTarget)) Then
                 If lngValuesFound = 1 Then
-                    fnLngLocateValueCol = rngMyCell.row
-                    Exit Function
+                    fnLngLocateValueCol = rngMyCell.Column
+                    If blnLookUpToBottom Then Exit Function
                 Else
                     Call Decrement(lngValuesFound)
                 End If
@@ -129,14 +145,12 @@ Public Function fnLngLocateValueCol(ByVal strTarget, _
             If strTarget = Trim(rngMyCell) Then
                 If lngValuesFound = 1 Then
                     fnLngLocateValueCol = rngMyCell.Column
-                    Exit Function
+                    If blnLookUpToBottom Then Exit Function
                 Else
                     Call Decrement(lngValuesFound)
                 End If
             End If
         End If
     Next rngMyCell
-    
-    fnLngLocateValueCol = -999
 
 End Function
