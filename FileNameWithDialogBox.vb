26fe862863f69b7726ecac3ac516644ee18d1d60
Vitosh Academy <Vitosh@users.noreply.github.com>
2017-06-20 15:28:52 +0200

    Update FileNameWithDialogBox.vb

--- FileNameWithDialogBox.vb
+++ FileNameWithDialogBox.vb
@@ -1,13 +1,26 @@
 Option Explicit
 
-Private Sub cmd_browse_Click()
+Public Sub MainBrowse(my_obj As Object)
     
-    Dim str_file As String
+    Dim str_file                As String
     
-    str_file = Application.GetOpenFilename _
-        (Title:="Please choose a file to open", _
-        FileFilter:="Excel Files *.xls* (*.xls*),")
+    str_file = Application.GetOpenFilename(Title:="Please choose a file to open", FileFilter:="Excel Files *.xls* (*.xls*),")
+    my_obj = str_file
+
+End Sub
+
+Private Sub btnBrowse_Click()
     
-    txtbox_display.Text = str_file
+    Dim strInitial      As String
+    Dim objLabel        As Object
     
+    Set objLabel = ThisWorkbook.Worksheets(tbl_input.Name).lblDisplay
+    
+    strInitial = objLabel
+    Call MainBrowse(objLabel)
+
+    If Len(objLabel) >= 6 Then 'Falsch, False
+        objLabel = strInitial
+    End If
+
 End Sub
