7f57343b970795d835fba394a65c36043f0b5236
Vitosh Academy <Vitosh@users.noreply.github.com>
2017-07-31 17:08:00 +0200

    Update MakeValuesSelection.vb

--- Excel TDD/MakeValuesSelection.vb
+++ Excel TDD/MakeValuesSelection.vb
@@ -1,27 +1,50 @@
-'Select and run, values are printed
+'---------------------------------------------------------------------------------------
+' Method : MakeAllValues
+' Author : v.doynov
+' Date   : 07.11.2016
+' Purpose: Select the range, for which you want the TDD code.
+' Make sure that you can compile!!! (CreateLogFile and change_commas)
+'---------------------------------------------------------------------------------------
+Public Sub MakeAllValues()
 
-Public Sub MakeValues()
-
-    Dim my_cell         As Range
-    Dim str             As String
-    Dim strAll          As String
-    Dim l_counter       As Long
-
+    Dim my_cell                 As Range
+    Dim l_counter               As Long
+    Dim str                     As String
+    Dim str_result              As String
+    
+    STR_ERROR_REPORT = ""
+    
     For Each my_cell In Selection
         Call Increment(l_counter)
-        str = "my_arr(" & l_counter & ")= "
-
+        str = vbTab & "my_arr(" & l_counter & ")= "
+        
         If Len(my_cell) > 0 Then
-            str = str & change_commas(my_cell.value)
+            If IsDate(my_cell) Then
+                str = str & "CDate(""" & my_cell & """)"
+            Else
+                If Not IsNumeric(my_cell) Then
+                    str = str & """" & my_cell & """"
+                Else
+                    str = str & change_commas(my_cell.value)
+                End If
+            End If
         Else
-            str = str & 0
+            If my_cell.HasFormula Then
+                str = str & """"""
+            Else
+                str = str & 0
+            End If
         End If
         
-        strAll = IIf(Len(strAll), strAll & vbCrLf & str, str)
-        Debug.Print str
-
+        If Len(str_result) = 0 Then
+            str_result = str
+        Else
+            str_result = str_result & vbCrLf & str
+        End If
+        
     Next my_cell
     
-    CreateLogFile (strAll)
+    Debug.Print str_result
+    Call CreateLogFile(str_result)
 
 End Sub
