40d8424cbef1b168efa1a70fec5b5ee07ac471c4
Vitosh Academy <Vitosh@users.noreply.github.com>
2017-08-17 11:25:33 +0200

    Update Formula.vb

--- Formula.vb
+++ Formula.vb
@@ -12,3 +12,19 @@ Public Sub PrintMeUsefulFormula()
     Debug.Print strFormula
 
 End Sub
+
+'A bit untested, use with caution --------v
+Public Sub PrintMeUsefulFormat()
+
+    Dim strFormula  As String
+    Dim strParenth  As String
+
+    strParenth = """"
+
+    strFormula = Selection.NumberFormat
+    strFormula = Replace(strFormula, """", """""")
+
+    strFormula = strParenth & strFormula & strParenth
+    Debug.Print strFormula
+
+End Sub
