8c0c2fb0bec9e7bf8296a2bf4ad922e40e636ee1
Vitosh Academy <Vitosh@users.noreply.github.com>
2017-10-10 11:06:05 +0200

    Update Preprocessor.vb

--- Preprocessor.vb
+++ Preprocessor.vb
@@ -1,41 +1,31 @@
 Option Explicit
 
 #If Win32 Then
-
     Sub MyTest()
         Debug.Print "32 bits."
-    End Sub
-    
+    End Sub    
 #ElseIf Win64 Then
-
     Sub MyTest()
         Debug.Print "64 bits."
         'This should be an error only if it is 64 bits:
         Debug.Print 0 / 0
-    End Sub
-    
+    End Sub    
 #ElseIf Win16
-
     Sub MyTest()
         Debug.Print "16 bits."
-    End Sub
-    
+    End Sub    
 #End If
 
-Sub MyExecutiveMain()
-    
+Sub MyExecutiveMain()    
     MyTest
-
 End Sub
 
 Sub WhichVersion()
-
     #If VBA7 Then
         Debug.Print "VBA7"
     #Else
         Debug.Print "NOT VBA7"
     #End If
-
 End Sub
 
 #If VBA7 And Win64 Then
@@ -43,4 +33,3 @@ End Sub
 #Else
     Private Declare Sub Sleep Lib "kernel32" (ByVal dwMilliseconds As Long)
 #End If
-
