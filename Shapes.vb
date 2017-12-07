bcf7f702a47ecd1d5847cf3ac9a9cd7933e0eff1
Vitosh Academy <Vitosh@users.noreply.github.com>
2017-12-07 14:25:55 +0100

    Update Shapes.vb

--- Shapes.vb
+++ Shapes.vb
@@ -60,3 +60,57 @@ Sub translatorField_Klicken()
     End If
 
 End Sub
+
+'---------------------------------------------------------------------------------------------------------------------------------
+'---------------------------------------------------------------------------------------------------------------------------------
+'---------------------------------------------------------------------------------------------------------------------------------
+
+Option Explicit
+
+Sub TestMe()
+
+    Dim shp             As Shape
+    Dim arrOfShapes()   As Variant
+
+    With ActiveSheet
+        For Each shp In .Shapes
+            If InStrB(shp.Name, "Rec") > 0 Then
+                arrOfShapes = incrementArray(arrOfShapes, shp.Name)
+            End If
+        Next
+        If IsArrayAllocated(arrOfShapes) Then
+            Debug.Print .Shapes.Range(arrOfShapes(0)).Name
+            .Shapes.Range(arrOfShapes).Delete
+        End If
+    End With
+End Sub
+
+
+Public Function incrementArray(arrOfShapes As Variant, nameOfShape As String) As Variant
+
+    Dim cnt         As Long
+    Dim arrNew      As Variant
+
+    If IsArrayAllocated(arrOfShapes) Then
+        ReDim arrNew(UBound(arrOfShapes) + 1)            
+        For cnt = LBound(arrOfShapes) To UBound(arrOfShapes)
+            arrNew(cnt) = CStr(arrOfShapes(cnt))
+        Next cnt
+        arrNew(UBound(arrOfShapes) + 1) = CStr(nameOfShape)
+    Else
+        arrNew = Array(nameOfShape)
+    End If
+
+    incrementArray = arrNew
+
+End Function
+
+Function IsArrayAllocated(Arr As Variant) As Boolean
+    On Error Resume Next
+    IsArrayAllocated = IsArray(Arr) And _
+                       Not IsError(LBound(Arr, 1)) And _
+                       LBound(Arr, 1) <= UBound(Arr, 1)
+
+End Function
+Credits to this guy for the finding that the arrOfShapes should be declared with parenthesis (I have spent about 30 minutes researching why I could not pass it correctly) and to CPearson for the IsArrayAllocated().
+
