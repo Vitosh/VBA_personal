7a1d8da34d68df53fd264bf4e8abfa8f2b4161a5
Vitosh Academy <Vitosh@users.noreply.github.com>
2018-01-08 17:57:19 +0100

    Update ByReferenceByValue.vb

--- ByReferenceByValue.vb
+++ ByReferenceByValue.vb
@@ -2,7 +2,7 @@ Option Explicit
 
 Public Sub TestMe()
 
-    Dim var1, var2, var3, var4, var5, var6
+    Dim var1, var2, var3, var4, var5, var6, var7, var8
     
     var1 = Array(0, 1)
     var2 = Array(10, 11)
@@ -10,30 +10,34 @@ Public Sub TestMe()
     var4 = Array(1000, 1001)
     var5 = Array(10000, 10001)
     var6 = Array(100000, 100001)
+    var7 = Array(1000000, 10000001)
+    var8 = Array(10000000, 100000001)
     
-    'ByReference
-    var1 = increment01(var1)
-    var1 = increment01(var1)
+    increment01 (var1)
+    increment01 (var1)
     
-    'ByReference
-    var2 = increment02(var2)
-    var2 = increment02(var2)
+    increment02 (var2)
+    increment02 (var2)
     
-    'ByReference
-    increment01 var3
-    increment01 var3
+    increment03 (var3)
+    increment03 (var3)
     
-    'ByValue
-    increment02 var4
-    increment02 var4
+    increment04 (var4)
+    increment04 (var4)
     
-    'ByReference
-    increment03 var5
-    increment03 var5
+    'ByReference!
+    increment01 var5
+    increment01 var5
+
+    increment02 var6
+    increment02 var6
     
-    'ByValue
-    increment04 var6
-    increment04 var6
+    'ByReference!
+    increment03 var7
+    increment03 var7
+    
+    increment04 var8
+    increment04 var8
     
     Debug.Print var1(0)
     Debug.Print var2(0)
@@ -41,6 +45,8 @@ Public Sub TestMe()
     Debug.Print var4(0)
     Debug.Print var5(0)
     Debug.Print var6(0)
+    Debug.Print var7(0)
+    Debug.Print var8(0)
 
 End Sub
 
@@ -72,11 +78,12 @@ Public Function increment04(ByVal testValue As Variant) As Variant()
     
 End Function
 
- '2 
- '12 
- '102 
- '1000 
- '10002 
- '100000 
-
-
+'Result in the immediate window:
+ 0 
+ 10 
+ 100 
+ 1000 
+ 10002 
+ 100000 
+ 1000002 
+ 10000000 
