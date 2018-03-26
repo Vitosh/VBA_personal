d82922ca6d32ae6daa8f860893519ab36abe5a10
Vitosh Academy <Vitosh@users.noreply.github.com>
2018-03-26 18:40:18 +0200

    Update DoubleCalculation.vb

--- Financial/DoubleCalculation.vb
+++ Financial/DoubleCalculation.vb
@@ -14,3 +14,19 @@ Sub TestMe()
     Range("A5").Formula = "=A3=A4"
     
 End Sub
+
+Sub TestMe2()
+    
+    Dim a           As Double: a = 20
+    Dim b           As Double: b = 0.1
+    
+    Cells.Clear
+    Range("A1").Formula = "=" & a & "+0.1"
+    Range("A2") = a
+    
+    Range("A3").Formula = "=A1-A2"
+    Range("A4") = b
+    Range("A5").Formula = "=A3=A4"
+    
+End Sub
+
