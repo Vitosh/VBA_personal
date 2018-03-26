54137ec89776a680849e96132e93893138692144
Vitosh Academy <Vitosh@users.noreply.github.com>
2018-03-26 18:34:28 +0200

    Update DoubleCalculation.vb

--- Financial/DoubleCalculation.vb
+++ Financial/DoubleCalculation.vb
@@ -1,4 +1,16 @@
-35
- 0,10
-34,90
- 0,10
+'double inaccuracy example example double inaccuracy floating point accuracy
+
+Sub TestMe()
+    
+    Dim a           As Double: a = 20
+    Dim b           As Double: b = 0.1
+    
+    Cells.Clear
+    Range("A1") = a - b
+    Range("A2") = a + b
+    
+    Range("A3").Formula = "=A1-A2"
+    Range("A4") = b * 2 * -1
+    Range("A5").Formula = "=A3=A4"
+    
+End Sub
