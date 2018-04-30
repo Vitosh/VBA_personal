d3ee67aa8280b57a1d7091d4185e87c2eb41f4c8
Vitosh Academy <Vitosh@users.noreply.github.com>
2018-04-30 22:40:29 +0200

    Update ConditionalFormat.vb

--- ConditionalFormat.vb
+++ ConditionalFormat.vb
@@ -1,25 +1,23 @@
 Sub ListAllConditionalFormat()
-    
+
     Dim cf      As FormatCondition
     Dim ws      As Worksheet
     Dim l       As Long
     Dim rngCell As Range
-    
+
     On Error Resume Next
-    
+
     Application.EnableEvents = False
     Application.ScreenUpdating = False
-    tblPrint.Cells.Clear
-    
+    tblReport.Cells.Clear
+
     For Each ws In ThisWorkbook.Worksheets
         Debug.Print ws.Name
-        
+
         For Each cf In ws.Cells.FormatConditions
             l = 1 + l
-            
-            With tblPrint
-                
-                Set rngCell = Cells(l, 1)
+            With tblReport
+                Set rngCell = .Cells(l, 1)
                 rngCell = cf.AppliesTo.Address
                 rngCell.Offset(0, 1) = cf.Type
                 rngCell.Offset(0, 2) = "'" & cf.Formula1
@@ -28,11 +26,9 @@ Sub ListAllConditionalFormat()
                 rngCell.Offset(0, 5) = ws.Name
                 rngCell.Offset(0, 6) = "'" & cf.AppliesTo.AddressLocal
                 rngCell.Offset(0, 7) = "'" & cf.Formula2
-                
-                
             End With
         Next cf
     Next ws
     Debug.Print "END!"
-    
+
 End Sub
