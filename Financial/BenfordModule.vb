da70c25a870fbbc6f39155f5476fa3e276294c85
Vitosh Academy <Vitosh@users.noreply.github.com>
2019-06-23 19:28:06 +0200

    Update BenfordModule.vb

--- Financial/BenfordModule.vb
+++ Financial/BenfordModule.vb
@@ -1,67 +1,49 @@
 Option Explicit
 
-Public STR_ZERO_VALUES      As String
-Public STR_UNKNOWN_VALUES   As String
-Public STR_ERROR_REPORT     As String
-
-Public Sub CheckSelection(Optional rngMyRange As Range)
+Public Sub MainBenfordCheck(myRange As Range)
     
-    Dim rngCell     As Range
-    Dim objCheck    As New clsCheckDigits
-    
-    STR_ZERO_VALUES = ""
-    STR_UNKNOWN_VALUES = ""
-    
-    If rngMyRange Is Nothing Then Set rngMyRange = Selection
-    Debug.Print rngMyRange.Address
-    
-    For Each rngCell In rngMyRange
-        If Len(rngCell) > 1 And IsNumeric(rngCell) Then
-            If rngCell > 1 Then
-                objCheck.IncrementValue rngCell, "", ""
-                objCheck.IncrementCount
-            End If
+    Dim myCell     As Range
+    Dim benford    As New BenfordModel
+            
+    For Each myCell In myRange
+        If IsNumeric(myCell) Then
+            benford.IncrementValue Abs(myCell.value)
+            benford.IncrementCount
         End If
-    Next rngCell
+    Next myCell
     
-    CreateLogFile objCheck.CreateBenfordLawReport, objCheck
-    
+    CreateLogFile benford.CreateBenfordLawReport
     
 End Sub
 
-
-Public Sub CreateLogFile(Optional str_print As String, Optional objCheckDigits As clsCheckDigits)
+Public Sub CreateLogFile(Optional report As String)
 
     On Error GoTo CreateLogFile_Error
-
-    Dim fs                  As Object
-    Dim obj_text            As Object
-    Dim str_filename        As String
-    Dim str_new_file        As String
-    Dim str_shell           As String
     
-    str_new_file = "\tests_info"
-
-    STR_ERROR_REPORT = STR_ERROR_REPORT & vbCrLf & vbCrLf & Now & vbCrLf & "Erstellt von: " & Environ("USERNAME")
-
-    str_filename = ThisWorkbook.Path & str_new_file & codify_time(True)
-    If Dir(ThisWorkbook.Path & str_new_file, vbDirectory) = vbNullString Then MkDir ThisWorkbook.Path & str_new_file
-
+    Dim newFilePath As String
+    newFilePath = "\tests_info"
+     
+    Dim filename As String
+    filename = ThisWorkbook.Path & newFilePath & CodifyTime(True)
+    If Dir(ThisWorkbook.Path & newFilePath, vbDirectory) = vbNullString Then MkDir ThisWorkbook.Path & newFilePath
+    
+    Dim fs  As Object
     Set fs = CreateObject("Scripting.FileSystemObject")
-    Set obj_text = fs.CreateTextFile(str_filename, True)
+    
+    Dim notepad As Object
+    Set notepad = fs.CreateTextFile(filename, True)
 
-    If Len(STR_ERROR_REPORT) > 1 Then
-        obj_text.writeline (STR_ERROR_REPORT)
-    Else
-        obj_text.writeline (str_print)
-    End If
-        
-    obj_text.writeline (objCheckDigits.CreateBenfordLawReport)
-    obj_text.Close
-
-    str_shell = "C:\WINDOWS\notepad.exe "
-    str_shell = str_shell & str_filename
-    Call Shell(str_shell)
+    Dim header  As String
+    header = Now & vbCrLf & "Created by: " & Environ("USERNAME")
+    
+    notepad.WriteLine header
+    notepad.WriteLine report
+    notepad.Close
+    
+    Dim shellCommand        As String
+    shellCommand = "C:\WINDOWS\notepad.exe "
+    shellCommand = shellCommand & filename
+    Shell shellCommand
 
     On Error GoTo 0
     Exit Sub
@@ -72,23 +54,20 @@ CreateLogFile_Error:
 
 End Sub
 
-
-Public Function codify_time(Optional b_make_str As Boolean = False) As String
+Public Function CodifyTime(Optional makePath As Boolean = False) As String
 
     On Error GoTo codify_Error
 
-    Dim dbl_01 As Variant
-    Dim dbl_02 As Variant
-    Dim dbl_now As Double
+    Dim timePart01 As Double
+    Dim timePart02 As Double
+    Dim timePartNow As Double
 
-    dbl_now = Round(Now(), 8)
+    timePartNow = Round(Now(), 8)
+    timePart01 = Split(CStr(timePartNow), ",")(0)
+    timePart02 = Split(CStr(timePartNow), ",")(1)
+    CodifyTime = Hex(timePart01) & "_" & Hex(timePart02)
 
-    dbl_01 = Split(CStr(dbl_now), ",")(0)
-    dbl_02 = Split(CStr(dbl_now), ",")(1)
-
-    codify_time = Hex(dbl_01) & "_" & Hex(dbl_02)
-
-    If b_make_str Then codify_time = "\" & codify_time & ".txt"
+    If makePath Then CodifyTime = "\" & CodifyTime & ".txt"
 
     On Error GoTo 0
     Exit Function
@@ -98,3 +77,4 @@ codify_Error:
     MsgBox "Error " & Err.Number & " (" & Err.Description & ") in procedure codify of Function TDD_Export"
 
 End Function
+
