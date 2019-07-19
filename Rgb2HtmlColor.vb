f2b8c124eeab63f4b5d04e476cf028e2c0450fb8
Vitosh Academy <Vitosh@users.noreply.github.com>
2019-07-19 20:04:00 +0200

    Update Rgb2HtmlColor.vb

--- Rgb2HtmlColor.vb
+++ Rgb2HtmlColor.vb
@@ -33,3 +33,30 @@ Public Function RGB2HTMLColor(B As Byte, G As Byte, R As Byte) As String
 ErrorHandler:
     Debug.Print "RGB2HTMLColor was not successful"
 End Function
+
+Sub GetHexFromInteriorCell()
+
+    Worksheets(1).Cells(1, "A").Interior.Color = vbYellow
+    Debug.Print Hex(Worksheets(1).Cells(1, "A").Interior.Color)  'FFFF
+    Debug.Print Worksheets(1).Cells(1, "A").Interior.Color       '65535
+
+    Dim hexColor As String
+    hexColor = Right("000000" & Hex(Worksheets(1).Cells(1, "A").Interior.Color), 6)
+
+    Debug.Print HexToRgb(hexColor)                               'FFFF00
+
+End Sub
+
+Public Function HexToRgb(hexColor As String) As String
+
+    Dim red As String
+    Dim green As String
+    Dim blue As String
+
+    red = Left(hexColor, 2)
+    green = Mid(hexColor, 3, 2)
+    blue = Right(hexColor, 2)
+
+    HexToRgb = blue & green & red
+
+End Function
