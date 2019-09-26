31676296adc9b30fd71f7359d6a21a345a9122c2
Vitosh Academy <Vitosh@users.noreply.github.com>
2019-09-26 17:41:18 +0300

    Create RangeConnector.vb

---
+++ RangeConnector.vb
@@ -0,0 +1,29 @@
+Sub TestMe()
+
+    Dim myRange As Range
+    Set myRange = Selection
+    
+    Dim l As Long
+    Dim t As Long
+    Dim w As Long
+    Dim h As Long
+    
+    l = myRange.Left
+    t = myRange.Top
+    w = myRange.Width
+    h = myRange.Height
+    
+    ActiveSheet.Shapes.AddConnector msoConnectorStraight, l, t, l + (w) / 2, t
+    ActiveSheet.Shapes.AddConnector msoConnectorStraight, l, t, l, t
+    
+    Set myRange = myRange.Offset(1)
+    l = myRange.Left
+    t = myRange.Top
+    w = myRange.Width
+    h = myRange.Height
+    ActiveSheet.Shapes.AddConnector(msoConnectorStraight, l, t, l + (w) / 2, t).Select
+    
+
+    myRange.Select
+
+End Sub
