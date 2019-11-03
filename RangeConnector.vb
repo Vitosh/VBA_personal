d6d1ddb78c74dadff1d408a59394a845eb94080b
Vitosh Academy <Vitosh@users.noreply.github.com>
2019-11-03 23:09:47 +0100

    Update RangeConnector.vb

--- RangeConnector.vb
+++ RangeConnector.vb
@@ -37,3 +37,38 @@ Sub FormatHalfOfTheSelectedCell()
     End With
 
 End Sub
+                
+                
+Sub FormatRightPartOfSelectedCell()
+
+    Dim myRange As Range
+    Dim color As Long: color = RGB(0, 0, 0)
+    Dim myShape As Shape
+    
+    With Worksheets("Sheet1") 'With ActiveSheet
+        Set myRange = .Range("E10") 'Selection
+        Dim left As Long: left = myRange.left
+        Dim top As Long: top = myRange.top
+        Dim width As Long: width = myRange.width
+        Dim heigth As Long: heigth = myRange.Height
+
+        'Top line:
+        Set myShape = .Shapes.AddConnector(msoConnectorStraight, left + (width) / 2, top, left + width, top)
+        myShape.Line.ForeColor.RGB = color
+        
+        'Right line
+        Set myShape = .Shapes.AddConnector(msoConnectorStraight, left + width, top, left + width, top + myRange.Height)
+        myShape.Line.ForeColor.RGB = color
+                
+        Set myRange = myRange.Offset(1)
+        left = myRange.left
+        top = myRange.top
+        width = myRange.width
+        heigth = myRange.Height
+                
+        'Bottom Line:
+        Set myShape = .Shapes.AddConnector(msoConnectorStraight, left + (width) / 2, top, left + width, top)
+        myShape.Line.ForeColor.RGB = RGB(200, 0, 0)
+    End With
+
+End Sub
