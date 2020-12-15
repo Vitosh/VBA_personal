62c2d069fdf15786dcf647fc8fda4be96177f622
Vitosh Academy <Vitosh@users.noreply.github.com>
2020-12-15 18:54:29 +0200

    Update FileExists.vb

--- FileExists.vb
+++ FileExists.vb
@@ -8,3 +8,15 @@ Public Function b_file_exists(ByVal str_file_path As String) As Boolean
     b_file_exists = (str_test <> "")
 
 End Function
+
+'works in eshare
+'eshare file exists
+
+Public Function EshareFileExists(filePath)
+    
+    filePath = Replace(filePath, "https:", "")
+    filePath = Replace(filePath, "%20", " ")
+    filePath = Replace(filePath, "/", "\")
+    FileExists = CreateObject("Scripting.FileSystemObject").FileExists(filePath)
+    
+End Function
