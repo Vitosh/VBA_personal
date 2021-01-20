33c75283338d3a3acbf92cc6e2e3bde335eb1816
Vitosh Academy <Vitosh@users.noreply.github.com>
2021-01-20 14:14:04 +0200

    Update DictionaryExample.vb

--- DictionaryExample.vb
+++ DictionaryExample.vb
@@ -51,3 +51,45 @@ Public Sub PrintDictionary(myDict As Object)
     Next key
     
 End Sub
+
+Public Sub NestedDictionaryExample()
+    
+    Dim outer As Dictionary
+    Dim inner As Dictionary
+    
+    Set outer = New Dictionary
+    
+    Dim i As Long
+    For i = 1 To 10
+        Set inner = New Dictionary
+        inner.Add 10 * i, "first" & i
+        inner.Add 100 * i, "second" & i
+        inner.Add 1000 * i, "third" & i
+        outer.Add i, inner
+    Next i
+    
+    Dim innerKey As Variant
+    Dim outerKey As Variant
+    
+    For Each outerKey In outer.Keys
+        Debug.Print "Outer key:"; outerKey
+        Debug.Print "Inner key: value"
+        'PrintDictionary outer(outerKey)
+        
+        For Each innerKey In outer(outerKey)
+            Debug.Print innerKey; ": "; outer(outerKey)(innerKey)
+        Next innerKey
+        Debug.Print "----------------"
+        
+    Next outerKey
+    
+End Sub
+
+Public Sub PrintDictionary(myDict As Object)
+    
+    Dim key     As Variant
+    For Each key In myDict.Keys
+        Debug.Print key; "-->"; myDict(key)
+    Next key
+    
+End Sub
