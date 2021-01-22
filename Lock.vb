be012c4eb061d85a3eb2bc71f2da5f96d34d7e46
Vitosh Academy <Vitosh@users.noreply.github.com>
2021-01-22 16:28:49 +0200

    Update Lock.vb

--- Lock.vb
+++ Lock.vb
@@ -1,21 +1,26 @@
+'lock cells, lock ranges, lock cells with formulas
 Sub ProtectCellsWithFormulas()
-    'lock cells, lock ranges, lock cells with formulas
+   
     Dim wks As Worksheet
     Dim myCell As Range
     
     For Each wks In ThisWorkbook.Worksheets
-        If wks.Name = tblForwinCrest.Name Or wks.Name = tblForwinCrestPrefilled.Name Then
-            For Each myCell In wks.Range("A1:R102").Cells
-                If myCell.MergeArea.Cells.Count = 1 Then
-                    If myCell.HasFormula Then
-                        myCell.Locked = True
-                    Else
-                        myCell.Locked = False
+        With wks
+            If .Name = tblForwinCrest.Name Or .Name = tblForwinCrestPrefilled.Name Then
+                .Unprotect "v"
+                For Each myCell In wks.Range("A1:R102").Cells
+                    If myCell.MergeArea.Cells.Count = 1 Then
+                        If myCell.HasFormula Then
+                            myCell.Locked = True
+                        Else
+                            myCell.Locked = False
+                        End If
                     End If
-                End If
-            Next myCell
-            wks.Protect "vitoshacademy"
-        End If
+                Next myCell
+                .EnableOutlining = True
+                .Protect "v", contents:=True, userinterfaceonly:=True
+            End If
+        End With
     Next wks
     
 
