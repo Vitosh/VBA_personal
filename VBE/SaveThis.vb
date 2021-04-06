084f07596bc48a84959665d6bde808aa5c44d3e7
Vitosh Academy <Vitosh@users.noreply.github.com>
2021-04-06 14:55:39 +0300

    Create SaveThis.vb

---
+++ VBE/SaveThis.vb
@@ -0,0 +1,38 @@
+Public Sub SaveThis()
+
+'saves foo.4.5.6.xlsb to foo.4.5.7.xlsb
+        
+    Dim mySplitter As Variant
+    mySplitter = Split(ThisWorkbook.FullName, ".")
+    
+    Dim oldVersion As String
+    oldVersion = mySplitter(UBound(mySplitter) - 1)
+    
+    Dim newVersion As String
+    newVersion = oldVersion + 1
+    
+    mySplitter(UBound(mySplitter) - 1) = newVersion
+    
+    Dim newName As String
+    newName = Join(mySplitter, ".")
+    
+    ThisWorkbook.SaveAs newName
+    Debug.Print newName
+    
+End Sub
+
+Public Sub SaveThisM()
+    
+'saves foo.4.5.6.xlsb to foo.4.5.7.xlsb
+'and moves the old one to root\Arch\Auto
+
+    Dim oldName As String
+    oldName = ThisWorkbook.Name
+    
+    SaveThis
+    
+    Dim fso As New FileSystemObject
+    fso.MoveFile Source:=ThisWorkbook.path & "\" & oldName, Destination:=ThisWorkbook.path & "\Arch\Auto\" & oldName
+    Debug.Print ThisWorkbook.path & "\Arch\Auto\" & oldName
+    
+End Sub
