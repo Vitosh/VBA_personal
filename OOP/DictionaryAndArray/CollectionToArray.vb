f568bc3dea2939c47b6e6074440ad5574839e812
Vitosh Academy <Vitosh@users.noreply.github.com>
2021-06-23 14:03:42 +0300

    Update CollectionToArray.vb

--- OOP/DictionaryAndArray/CollectionToArray.vb
+++ OOP/DictionaryAndArray/CollectionToArray.vb
@@ -3,7 +3,7 @@ Public Function CollectionToArray(myCol As Collection) As Variant
     Dim result  As Variant
     Dim cnt     As Long
     
-    ReDim result(myCol.Count)
+    ReDim result(myCol.Count - 1)
 
     For cnt = 0 To myCol.Count - 1
         result(cnt) = myCol(cnt + 1)
