0967770f1260d3a6eb9d2cdb60d6aa02c4785368
Vitosh Academy <Vitosh@users.noreply.github.com>
2022-02-24 14:46:23 +0200

    Update mod_Notepad.vb
    waiting a second is needed not to have a collision of the names of the txt file, if these are created too soon after each other.

--- ExcelTdd/mod_Notepad.vb
+++ ExcelTdd/mod_Notepad.vb
@@ -1,41 +1,53 @@
 Option Explicit
 
-Sub CreateLogFile(Optional str_print As String)
+Public Sub CreateLogFile(Optional report As String)
 
-    If SET_IN_PRODUCTION Then On Error GoTo CreateLogFile_Error
-
-    Dim fs                      As Object
-    Dim obj_text                As Object
-    Dim str_filename            As String
-    Dim str_new_file            As String
-    Dim str_shell               As String
-
-    str_new_file = "\tests_info"
-
-    str_filename = ThisWorkbook.Path & str_new_file & codify_time(True)
-    If Dir(ThisWorkbook.Path & str_new_file, vbDirectory) = vbNullString Then MkDir ThisWorkbook.Path & str_new_file
-
-    Set fs = CreateObject("Scripting.FileSystemObject")
-    Set obj_text = fs.CreateTextFile(str_filename, True)
-
-    If Len(STR_ERROR_REPORT) > 1 Then
-        obj_text.writeline (STR_ERROR_REPORT)
-    Else
-        obj_text.writeline (str_print)
-    End If
+    On Error GoTo CreateLogFile_Error
     
-    obj_text.Close
+    WaitASecond
+    Dim newFilePath As String
+    newFilePath = "\reports"
+    Dim fileName As String
+    fileName = ThisWorkbook.Path & newFilePath & CodifyMyTime(True)
+    If Dir(ThisWorkbook.Path & newFilePath, vbDirectory) = vbNullString Then MkDir ThisWorkbook.Path & newFilePath
+    
+    Dim fs  As Object
+    Set fs = CreateObject("Scripting.FileSystemObject")
+    
+    Dim notepad As Object
+    Set notepad = fs.CreateTextFile(fileName, True)
 
-    str_shell = "C:\WINDOWS\notepad.exe "
-    str_shell = str_shell & str_filename
-    Call Shell(str_shell)
-
+    notepad.WriteLine report
+    notepad.Close
+    
     On Error GoTo 0
     Exit Sub
 
 CreateLogFile_Error:
-
     Debug.Print "Error " & Err.Number & " (" & Err.Description & ") in procedure CreateLogFile of Sub mod_TDD_Export"
-    
 End Sub
 
+Public Function CodifyMyTime(Optional makepath As Boolean = False) As String
+
+    On Error GoTo codify_Error
+
+    Dim timePart01 As Double
+    Dim timePart02 As Double
+    Dim timePartNow As Double
+
+    timePartNow = Round(Now(), 8)
+    timePart01 = Split(CStr(timePartNow), ".")(0)
+    timePart02 = Split(CStr(timePartNow), ".")(1)
+    CodifyMyTime = Format(Now, "YYYYMMMDD_HHNNSS") & "_" & Hex(timePart01) & "_" & Hex(timePart02)
+
+    If makepath Then CodifyMyTime = "\" & CodifyMyTime & ".xml"
+    On Error GoTo 0
+    Exit Function
+
+codify_Error:
+    MsgBox "Error " & Err.Number & " (" & Err.Description & ") in procedure CodifyTime"
+End Function                        
+                        
+Public Sub WaitASecond()
+    Application.Wait (Now + TimeValue("00:00:01"))
+End Sub
