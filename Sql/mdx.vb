41f1100be5c5f1a89fd020b5ffc2555cff6e9874
Vitosh Academy <Vitosh@users.noreply.github.com>
2022-05-11 12:35:29 +0300

    Update mdx.vb

--- Sql/mdx.vb
+++ Sql/mdx.vb
@@ -1,7 +1,7 @@
 Sub GetTheMdx()
 
     Dim pvtTable As PivotTable
-  Set pvtTable = tblFoo.PivotTables(1)
+    Set pvtTable = tblFoo.PivotTables(1)
     Dim result As String
     result = pvtTable.MDX & "---END"
     Debug.Print result
