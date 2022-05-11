7005adb194b539e6256932255dae0232fccef240
Vitosh Academy <Vitosh@users.noreply.github.com>
2022-05-11 12:35:17 +0300

    Create mdx.vb

---
+++ Sql/mdx.vb
@@ -0,0 +1,9 @@
+Sub GetTheMdx()
+
+    Dim pvtTable As PivotTable
+  Set pvtTable = tblFoo.PivotTables(1)
+    Dim result As String
+    result = pvtTable.MDX & "---END"
+    Debug.Print result
+    
+End Sub
